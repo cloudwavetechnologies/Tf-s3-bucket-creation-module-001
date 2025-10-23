a23a633b5685e5e0825d1478ce7bb23dbe593583
cloudwavetechnologies <manjunath@cloudwavetechnologies.co.in>
2025-10-23 08:12:31 +0530

    added backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "s3-terraform-state-bucket-000"
-    key            = "Infra//s3-terraform.tfstate"
+    key            = "Infra/s3-terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
     dynamodb_table = "terraform-locks"
